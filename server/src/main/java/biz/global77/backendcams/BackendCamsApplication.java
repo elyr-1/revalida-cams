e8855594f26e4bea7d49acd2dc7e2b6e2082d078
elyr-1 <elyrick.manlagnit@gmail.com>
2022-09-27 18:18:03 +0800

    starter files

---
+++ server/src/main/java/biz/global77/backendcams/BackendCamsApplication.java
@@ -0,0 +1,13 @@
+package biz.global77.backendcams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCamsApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/biz/global77/backendcams/BackendCamsApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.global77.backendcams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
