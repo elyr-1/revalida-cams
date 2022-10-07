e780e44debf49985c35fcebb749ab99703affc98
JohnArima <john.salangsang20@gmail.com>
2022-10-07 10:56:50 +0800

    Updated Commit

--- client/src/examples/Navbars/DefaultNavbar/index.js
+++ client/src/examples/Navbars/DefaultNavbar/index.js
@@ -1,18 +1,3 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 import { useState, useEffect } from "react";
 
 // react-router components
@@ -114,7 +99,7 @@ function DefaultNavbar({ transparent, light, action }) {
           </MDTypography>
         </MDBox>
         <MDBox color="inherit" display={{ xs: "none", lg: "flex" }} m={0} p={0}>
-          <DefaultNavbarLink icon="donut_large" name="dashboard" route="/dashboard" light={light} />
+          <DefaultNavbarLink icon="donut_large" name="dashboard" route="/home" light={light} />
           <DefaultNavbarLink icon="person" name="profile" route="/profile" light={light} />
           <DefaultNavbarLink
             icon="account_circle"
--- client/src/layouts/tables/index.js
+++ client/src/layouts/tables/index.js
@@ -51,7 +51,7 @@ function Tables() {
                 coloredShadow="info"
               >
                 <MDTypography variant="h6" color="white">
-                  Courses
+                  Subjects
                 </MDTypography>
               </MDBox>
               <MDBox pt={3}>
