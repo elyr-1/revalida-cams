1df8259107a2a2d69b15ecb1771966280e5a5b38
Jasonn123-dev <110155189+Jasonn123-dev@users.noreply.github.com>
2022-10-21 09:12:54 +0800

    students update

--- client/src/layouts/faculty/students/infoData.js
+++ client/src/layouts/faculty/students/infoData.js
@@ -1,71 +1,108 @@
 /* eslint-disable react/prop-types */
-/* eslint-disable react/function-component-definition */
-import MDBox from "components/MDBox";
+import { useEffect, useState } from "react";
 import MDTypography from "components/MDTypography";
-import MDBadge from "components/MDBadge";
+import TableCell from "@mui/material/TableCell";
+import VisibilityRoundedIcon from "@mui/icons-material/VisibilityRounded";
+import EditRoundedIcon from "@mui/icons-material/EditRounded";
+import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import ViewStudentForm from "./form/ViewStudentForm";
+import EditStudentForm from "./form/EditStudentForm";
 
-export default function infoData() {
-  return {
-    columns: [
-      { Header: "student No", accessor: "studentNo", width: "15%", align: "left" },
-      { Header: "fullname", accessor: "fullName", width: "15%", align: "left" },
-      { Header: "course code", accessor: "subjectCode", width: "15%", align: "left" },
-      { Header: "subject", accessor: "subject", align: "left" },
-      { Header: "professor", accessor: "professor", align: "center" },
-      { Header: "units", accessor: "units", align: "center" },
-      { Header: "final grade", accessor: "finalGrade", align: "center" },
-      { Header: "remarks", accessor: "remarks", align: "center" },
-      { Header: "action", accessor: "action", align: "center" },
-    ],
+function Student({ student, onEditStudent, onDeleteStudent }) {
+  const [open, setOpen] = useState(false);
+  const [view, setView] = useState(false);
 
-    rows: [
-      {
-        studentNo: (
-          <MDTypography display="block" variant="button" color="text" fontWeight="medium">
-            PM-17-00617-A
-          </MDTypography>
-        ),
-        fullName: (
-          <MDTypography display="block" variant="button" color="text" fontWeight="medium">
-            Jayson Derollo
-          </MDTypography>
-        ),
-        subjectCode: (
-          <MDTypography display="block" variant="button" color="text" fontWeight="medium">
-            Math102
-          </MDTypography>
-        ),
-        subject: (
-          <MDTypography display="block" variant="button" fontWeight="medium">
-            Fundamentals of Algebra
-          </MDTypography>
-        ),
-        professor: (
-          <MDTypography display="block" variant="button" fontWeight="medium">
-            Jason Macabante
-          </MDTypography>
-        ),
-        units: (
-          <MDTypography display="block" variant="caption" color="text" fontWeight="medium">
-            3
-          </MDTypography>
-        ),
-        finalGrade: (
-          <MDTypography display="block" variant="caption" color="text" fontWeight="medium">
-            5.0
-          </MDTypography>
-        ),
-        remarks: (
-          <MDBox ml={-1}>
-            <MDBadge badgeContent="failed" color="error" variant="gradient" size="sm" />
-          </MDBox>
-        ),
-        action: (
-          <MDTypography component="a" href="#" variant="button" color="error" fontWeight="medium">
-            Edit
-          </MDTypography>
-        ),
-      },
-    ],
-  };
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const handleOpenView = () => setView(true);
+  const handleCloseView = () => setView(false);
+
+  useEffect(() => {
+    handleClose();
+  }, [student]);
+
+  return (
+    <>
+      <TableCell>
+        <MDTypography display="block" variant="button" color="text" fontWeight="medium">
+          {student.studentNo}
+        </MDTypography>
+      </TableCell>
+      <TableCell>
+        <MDTypography display="block" variant="button" color="text" fontWeight="medium">
+          {student.lastname}
+        </MDTypography>
+      </TableCell>
+      <TableCell>
+        <MDTypography display="block" variant="button" color="text" fontWeight="medium">
+          {student.firstname}
+        </MDTypography>
+      </TableCell>
+      <TableCell>
+        <MDTypography display="block" variant="button" color="text" fontWeight="medium">
+          {student.gender}
+        </MDTypography>
+      </TableCell>
+      <TableCell>
+        <MDTypography display="block" variant="button" color="text" fontWeight="medium">
+          {student.programCode}
+        </MDTypography>
+      </TableCell>
+      <TableCell>
+        <MDTypography display="block" variant="button" color="text" fontWeight="medium">
+          {student.yearlevel}
+        </MDTypography>
+      </TableCell>
+      <TableCell>
+        <MDTypography display="block" variant="button" color="text" fontWeight="medium">
+          {student.sem}
+        </MDTypography>
+      </TableCell>
+      <TableCell>
+        <MDTypography display="block" variant="button" color="text" fontWeight="medium">
+          {student.remarks}
+        </MDTypography>
+      </TableCell>
+      <TableCell align="center">
+        <ButtonGroup>
+          <Tooltip title="View Student Details" placement="top">
+            <IconButton onClick={handleOpenView}>
+              <VisibilityRoundedIcon color="secondary" />
+            </IconButton>
+          </Tooltip>
+          <Dialog open={view} onClose={handleCloseView} fullWidth>
+            <DialogContent>
+              <ViewStudentForm key={student.studentId} student={student} />
+            </DialogContent>
+          </Dialog>
+          <Tooltip title="Edit Student Details" placement="top">
+            <IconButton onClick={handleOpen}>
+              <EditRoundedIcon color="primary" />
+            </IconButton>
+          </Tooltip>
+          <Dialog open={open} onClose={handleClose} fullWidth>
+            <DialogContent>
+              <EditStudentForm
+                key={student.studentId}
+                student={student}
+                onEditStudent={onEditStudent}
+              />
+            </DialogContent>
+          </Dialog>
+          <Tooltip title="Delete Student" placement="top">
+            <IconButton onClick={() => onDeleteStudent(student.studentId)}>
+              <DeleteRoundedIcon color="error" />
+            </IconButton>
+          </Tooltip>
+        </ButtonGroup>
+      </TableCell>
+    </>
+  );
 }
+
+export default Student;
